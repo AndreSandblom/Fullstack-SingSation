d5e391f85fc8c5d0f62188d42070a744b7ed8c78
Dario Ostojic <darioostojic2@gmail.com>
2025-05-17 11:13:23 +0200

    Updated Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy /api/* to backend on port 3001
-      '/api': 'https://fullstack-singsation-test.onrender.com:3001'
-    }
-  }
+  plugins: [react()]
 })
